28ff92ebf97e386f233bf84fa353bf9f74d3f084
frsearto <frsearto@gmail.com>
2025-07-16 16:11:48 -0400

    a

--- src/modelo/vehiculo/VehiculoCarga.java
+++ src/modelo/vehiculo/VehiculoCarga.java
@@ -3,9 +3,17 @@ package modelo.vehiculo;
 public class VehiculoCarga extends Vehiculo {
     protected int cargaMaximaKg;
 
+    //para vehiculo de carga nuevo
     public VehiculoCarga(String patente, String marca, String modelo){
         super(patente,marca,modelo);
         this.disponible = true;
+        this.diasArriendo = 0;
         this.cargaMaximaKg = 0;
+    }
+
+    //para cargar vehiculos de carga desde db
+    public VehiculoCarga(String patente, String marca, String modelo, boolean disponible, int diasArriendo, int cargaMaximaKg){
+        super(patente,marca,modelo,disponible,diasArriendo);
+        this.cargaMaximaKg = cargaMaximaKg;
     }
 }
